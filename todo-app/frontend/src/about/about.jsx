0e2d72b90f3832fbab2f958fbd3d5ab2272b789a
Tiago Silva <tiagohs.199@gmail.com>
2022-04-06 14:27:19 -0300

    final todo-app

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import PageHeader from "../template/pageHeader";
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='' /> 
+        <h1>Teste</h1>
+    </div>
+)
